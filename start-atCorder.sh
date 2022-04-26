f4e86fa3b15753980e4587b6cd99fb2c09723516
kimitashoichi <kimi.syou.koma@gmail.com>
2022-04-26 16:13:15 +0900

    2022-04-26

--- start-atCorder.sh
+++ start-atCorder.sh
@@ -4,8 +4,26 @@ jst_year=$(date '+%Y')
 jst_year_month=$(date '+%Y-%m')
 jst_month_day=$(date '+%m-%d')
 
-cd $jst_year
-cd $jst_year_month && mkdir $jst_month_day && cd $jst_month_day
+if [ -e $jst_year ]; then
+    cd $jst_year
+else
+    mkdir $jst_year
+    cd $jst_year
+fi
+
+if [ -e $jst_year_monthr ]; then
+    cd $jst_year_month
+else
+    mkdir $jst_year_month
+    cd $jst_year_month
+fi
+
+if [ -e $jst_month_day ]; then
+    cd $jst_month_day
+else
+    mkdir $jst_month_day
+    cd $jst_month_day
+fi
 
 echo -n 回答する問題のタイトルを入力してください:
 read question
